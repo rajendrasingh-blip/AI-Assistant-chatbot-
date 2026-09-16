565e89f5687fc871b6171b553849f2665b309fce
rajshakya77143-hub <rajshakya77143@gmail.com>
2026-09-16 18:09:42 +0530

    formit ui text views
    ref #main

--- widget/src/api/api.ts
+++ widget/src/api/api.ts
@@ -10,17 +10,17 @@ export const fetchGeminiChat = async (query: Message,
 ) => {
     try {
         // const response = await axiosBase.post("/chat", { query, projectId, collegeCode, searchType })
-        const response = await axiosBase.post("/AI/ask", { question: query })
+        const response = await axiosBase.post("/AI/ask", { question: query.content })
 
         const data = response?.data?.data;
-        console.log(response ,
-            data.length,'res')
         if (!response ||
             !data.length) {
             return { data: "failed to fetch query response from database." }
         }
         const values = data?.map((item: Record<string, any>) => Object.values(item)[0]);
-        const finalData = values.toString()
+        const finalData = values
+            .map((name: string, index: number) => `${index + 1}. ${name}`)
+            .join("\n");
 
         return { data: finalData }
     }
